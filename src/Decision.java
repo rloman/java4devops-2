254af50e9d5d38f5945a9e77860f6170bb44ff05
Raymond Loman <raymond@carpago.nl>
2019-01-08 15:43:31 +0100

    Initial load

---
+++ src/Decision.java
@@ -0,0 +1,71 @@
+public class Decision {
+
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 5;
+
+        if (a > b) {
+            System.out.println("a is groter dan b");
+        } else {
+            System.out.println("a is niet groter dan b");
+        }
+
+        if ((b != 0 || a > 5) && a > b) {
+            System.out.println(" ... ");
+        }
+        System.out.println(a / b);
+
+        if(a>b) {
+            System.out.println("Ja");
+        }
+        else if(a != 0) {
+
+        }
+        else {
+
+        }
+
+        /*
+
+        0-4 => Peuter
+        5-6 => Kleuter
+        7-12 => Schoolkind
+        13-18 => Student
+        19+ => Bier
+
+         */
+
+        int age=7;
+        String label = null;
+        switch (age) {
+            case 0:
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+                label = "Peuter";
+                break;
+            case 5:
+            case 6:
+                label = "Kleuter";
+                break;
+            case 7:
+            case 8:
+            case 9:
+            case 10:
+            case 11:
+            case 12:
+                label = "Schoolkind";
+                break;
+            default:
+                label = "Onbekend";
+                break;
+
+        }
+        // => Hier spring ik heen na de break
+        System.out.println(label);
+
+
+
+    }
+}
---
+++ src/Operators.java
@@ -0,0 +1,60 @@
+import java.time.LocalDateTime;
+
+public class Operators {
+
+    public static void main(String[] args) {
+
+        System.out.print("Hello world!");
+        int leeftijd = 50;
+
+        leeftijd = leeftijd + 1;
+
+        leeftijd = leeftijd / 2;
+
+        leeftijd = leeftijd + 1;
+        leeftijd++;
+        ++leeftijd;
+
+        leeftijd = 25;
+
+        System.out.println(leeftijd++);
+        // leeftijd is hier 26
+
+        System.out.println(++leeftijd);
+        // print 27
+        // verhoogt hem eerst, en dan print ie
+
+        System.out.println(leeftijd);
+        leeftijd++;
+        leeftijd = -leeftijd;
+        String label = leeftijd < 50 ? "Junior" : "Senior";
+        System.out.println(label);
+
+        leeftijd = leeftijd + 3;
+        leeftijd = leeftijd + 4;
+
+        leeftijd += 4;
+        leeftijd -= 4;
+        leeftijd /= 4;
+        leeftijd *= 4;
+
+        System.out.println(leeftijd);
+
+        // for Leontine
+        LocalDateTime now = LocalDateTime.now();
+
+        int minutes = now.getMinute();
+        int hours = now.getHour();
+
+        System.out.println(hours);
+        System.out.println(minutes);
+
+        System.out.println(hours * 60 + minutes);
+
+        System.out.printf("Het is dan: %02d:%02d%n", hours, minutes);
+
+
+
+
+    }
+}
